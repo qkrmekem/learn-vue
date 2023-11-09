38f173c54b404f1555937c979ebf74764a4091c7
qkrmekem <magicofclown@naver.com>
2023-11-10 00:58:04 +0900

    todolist create, delete, read기능 일부 구현

--- vue-intermediate/vue-todo/src/components/TodoInput.vue
+++ vue-intermediate/vue-todo/src/components/TodoInput.vue
@@ -3,7 +3,7 @@
     <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
     <!-- <button v-on:click="addTodo">add</button> -->
     <span class="addContainer" v-on:click="addTodo">
-        <i class="fa fa-plus addBtn" aria-hidden="true"></i>
+        <i class="fas fa-plus addBtn" ></i>
     </span>
   </div>
 </template>
@@ -18,10 +18,20 @@ export default {
     },
     methods: {
         addTodo: function(){
-            // console.log(this.newTodoItem);
-            // 저장하는 로직
-            localStorage.setItem(this.newTodoItem,this.newTodoItem);
-            this.clearInput();
+            if(this.newTodoItem !== ''){
+                let obj ={
+                    completed: false,
+                    item: this.newTodoItem
+                };
+                // 저장하는 로직
+                // obj의 값을 string값으로 담음
+                // js의 object가 들어가기 때문에 어떤 값이 들어있는지 확인할 수 없음
+                // localStorage.setItem(this.newTodoItem,obj);
+                // obj가 string형태로 저장됨 
+                localStorage.setItem(this.newTodoItem,JSON.stringify(obj));
+                this.clearInput();
+            }
+            
         },
         clearInput: function(){
             this.newTodoItem = '';
@@ -36,6 +46,7 @@ input:focus{
 }
 .inputBox{
     display: flex;
+    flex-direction: row;
     align-items: center;
     background: white;
     height: 50px;
@@ -43,6 +54,10 @@ input:focus{
     border-radius: 5px;
 }
 .inputBox input {
+    height: 100%;
+    float: right;
+    /* 띄어쓰기 해야함!!! */
+    width: calc(100% - 3rem);
     border-style: none;
     font-size: 0.9rem;
 }
@@ -50,6 +65,7 @@ input:focus{
     float: right;
     background: linear-gradient(to right, #6478FB, #8763FB);
     display: block;
+    height: 100%;
     width: 3rem;
     border-radius: 0 5px 5px 0;
 }
--- vue-intermediate/vue-todo/src/components/TodoList.vue
+++ vue-intermediate/vue-todo/src/components/TodoList.vue
@@ -1,15 +1,95 @@
 <template>
   <div>
-    list
+    <ul>
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+        <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" 
+        v-on:click="toggleComplete(todoItem,index)"></i>
+        <span v-bind:class="{textCompleted: todoItem.completed}">{{todoItem.item}}</span>
+        
+        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+          <i class="fas fa-trash-alt"></i>
+        </span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
+  data: function(){
+    return {
+      todoItems: []
+    }
+  },
+  methods: {
+    removeTodo: function(todoItem, index){
+      console.log(todoItem, index);
+      localStorage.removeItem(todoItem);
+      // index위치부터 1개의 요소를 제거
+      // splice는 요소를 제거하고 배열에 반영
+      // slice는 저장하지 않음
+      this.todoItems.splice(index,1);
+    },
+    toggleComplete: function(todoItem, index){
+      console.log(todoItem,index);
+      todoItem.completed = !todoItem.completed;
+      // 로컬스토리지 갱신
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item,JSON.stringify(todoItem.item));
+    }
+  },
+  // 라이프사이클 훅
+  // 컴포넌트가 생성되고 소멸되는 사이의 각 단계에서
+  // 실행되는 함수를 뜻함(총 8가지의 상태가 있음)
+  created: function(){
+    if(localStorage.length > 0){
+      for(let i = 0; i < localStorage.length; i++){
+        if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+          // 앞서 데이터를 로컬스토리지에 저장할 때 string형태로 저장했기 때문에
+          // JSON.parse로 다시 object타입으로 바꿔야함
+          console.log(JSON.parse(localStorage.getItem(localStorage.key(i))));
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
 
+          // this.todoItems.push(localStorage.key(i));
+        }
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+ul{
+  /* 리스트의 점을 제거함 */
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li{
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn{
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted{
+  color: #b3adad;
+}
+.textCompleted{
+  text-decoration: line-through;
+  color: #b3adad;
+}
+.removeBtn{
+  margin-left: auto;
+  color: #de4343;
+}
 </style>
